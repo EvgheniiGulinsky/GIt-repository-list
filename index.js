4f1302f64c78fb4cd31c727f0b9813f039510be2
Evgeni <riggedman22@gmail.com>
2021-02-01 16:30:51 +0300

    Fix long functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const form = document.getElementById('form')
 const input = document.getElementById('userName')
 const autoComplete = document.getElementsByClassName('auto-complete')[0]
+const BASE_URL = 'https://api.github.com/search/repositories'
 
 const debounce = (fn, debounceTime) => {
     let isCoolDown;
@@ -15,7 +16,7 @@ const debounceSearch = debounce(search, 500)
 input.addEventListener('input', debounceSearch)  
 
 async function searchRepos(name) {
-    const response = await fetch(`https://api.github.com/search/repositories?q=${name}`)
+    const response = await fetch(`${BASE_URL}?q=${name}`)
     const result = await response.json()
     return(result)
 }
@@ -31,24 +32,26 @@ async function search() {
             const repo = document.createElement('li')
             repo.className = `auto-complete__item`
             repo.innerHTML = result.items[i].name
-            repo.addEventListener('click', (e)=> {
-                if(!e.target.classList.contains('disabled')){
-                    e.target.classList.add('disabled')
-                    const addedRepo = document.createElement('div')
-                    addedRepo.className = 'added__item'
-                    addedRepo.id = result.items[i].owner.login
-                    addedRepo.innerHTML = ` Name: ${result.items[i].name} \n Owner: ${result.items[i].owner.login} \n Stars: ${result.items[i].stargazers_count}`
-                    const removeBtn = document.createElement('div')
-                    removeBtn.className = 'remove-repo'
-                    addedRepo.appendChild(removeBtn)
-                    input.value = ''
-                    autoComplete.innerHTML = ''
-                    removeBtn.addEventListener('click', (e) => {
-                        document.getElementById(e.target.parentNode.id).remove()
-                    })
-                document.getElementsByClassName('added')[0].appendChild(addedRepo)}
-            })
+            repo.addEventListener('click', addRepo.bind(this, result, i))
             autoComplete.appendChild(repo)
         }
     }
+}
+
+function addRepo (result, i) {
+    const addedRepo = document.createElement('div')
+    addedRepo.className = 'added__item'
+    addedRepo.id = result.items[i].owner.login
+    addedRepo.innerHTML = ` Name: ${result.items[i].name} \n Owner: ${result.items[i].owner.login} \n Stars: ${result.items[i].stargazers_count}`
+    const removeBtn = document.createElement('div')
+    removeBtn.className = 'remove-repo'
+    addedRepo.appendChild(removeBtn)
+    input.value = ''
+    autoComplete.innerHTML = ''
+    removeBtn.addEventListener('click', remove)
+    document.getElementsByClassName('added')[0].appendChild(addedRepo)
+}
+
+function remove(e) {
+    document.getElementById(e.target.parentNode.id).remove()
 }
